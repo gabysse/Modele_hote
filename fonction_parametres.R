ecf6c55035be90530ae01ca4e1d5ff84e96af27e
rallart <rallart@rals>
2017-03-01 11:19:34 +0100

    Modification dans la nouvelle branche du fichier qui appelle les paramètre et les met sous forme d'une liste param_to ainsi que l'ajout de param_to en tant que paramètre de la fonction qui nous permet d'avoir param_k

--- fonction_parametres.R
+++ fonction_parametres.R
@@ -1,17 +1,19 @@
 #### Fonction permettant de créer la liste param_k
+## Source des types de fonctions et variables deffinissant TO 
+# source("parametres/valeurs_parametres_puissance.R") #Contient les variables definissant les TO.
+# source("parametres/expression_parametres_puissance.R") #Contient les fonctions exprimant les TO.
+## A terme, remplacé par param_to dans la liste des arguments de la fonction
 
-func_param <- function(k){
-  # Source des types de fonctions et variables deffinissant TO 
-  source("parametres/valeurs_parametres_puissance.R") #Contient les variables definissant les TO.
-  source("parametres/expression_parametres_puissance.R") #Contient les fonctions exprimant les TO.
-  # A terme, remplacé par param_to dans la liste des arguments de la fonction
+
+func_param <- function(k, param_to){
+ 
   
-  betak <- beta(beta0,beta_max,P_beta,k)
-  ak <- a(a0,a_max,P_a,k)
-  alphak <- alpha(alpha0,alpha_max,P_alpha,k)
-  gammak <- gamma(gamma0,gamma_max,P_gamma,k)
-  bk <-b(b0,b_max,P_b,k)
-  qk <- cu(ak,bk,K)
+  betak <- beta(param_to$beta0,param_to$beta_max,param_to$P_beta,k)
+  ak <- a(param_to$a0,param_to$a_max,param_to$P_a,k)
+  alphak <- alpha(param_to$alpha0,param_to$alpha_max,param_to$P_alpha,k)
+  gammak <- gamma(param_to$gamma0,param_to$gamma_max,param_to$P_gamma,k)
+  bk <-b(param_to$b0,param_to$b_max,param_to$P_b,k)
+  qk <- cu(param_to$ak,param_to$bk,param_to$K)
   
   return(list(betak=betak,ak=ak,alphak=alphak,gammak=gammak,bk=bk,qk=qk))
 }
--- parametres/valeurs_parametres_puissance.R
+++ parametres/valeurs_parametres_puissance.R
@@ -20,3 +20,5 @@ b_max=1
 P_b=0.5
 
 K=10
+
+param_to <- list(a0=a0, alpha0=alpha0, alpha_max=alpha_max, a_max=a_max, b0=b0, beta0=beta0, beta_max=beta_max, b_max=b_max, gamma0=gamma0, gamma_max=gamma_max, K=K, P_a=P_a, P_alpha=P_alpha, P_b=P_b, P_beta=P_beta, P_gamma=P_gamma)
